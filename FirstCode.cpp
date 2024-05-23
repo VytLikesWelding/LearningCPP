1b3f49f57d1bbfe4aee9824b9f2612b77f2b043f
VytLikesWelding <vytsbuis@gmail.com>
2024-05-24 00:15:10 +0300

    init commit

---
+++ FirstCode.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+int main(){
+	
+	int a;
+	ifstream fin("duomenys.txt");
+	
+	fin >> a;
+	cout<< "\n" << a << "\n\n";
+	
+	fin.close();
+	return 0;
+}
